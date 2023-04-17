9e48e3fd0b8644787b17db81d245e470700c59d1
snapp <snapp@snapp.co>
2023-04-17 17:40:18 +0300

    added input system

---
+++ Assets/Scripts/Player/PlayerControll.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerControll : MonoBehaviour
+{
+    private PlayerInput _playerInput;
+    private IJumpPlayer _jumpPlayer;
+    private void Awake()
+    {
+        _playerInput = new PlayerInput();
+        _playerInput.Player.Jump.performed += ctx => Jump();
+    }
+
+    private void OnEnable()
+    {
+        _playerInput.Enable();
+    }
+    private void OnDisable()
+    {
+        _playerInput.Disable();
+    }
+
+    void Jump()
+    {
+      
+    }
+    
+}
