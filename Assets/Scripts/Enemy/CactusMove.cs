fd038b8f4deda32ebdce7d0217d7ba1f0d1d007a
snapp <snapp@snapp.co>
2023-04-17 19:22:42 +0300

    Enemy move and enemys have colliders

---
+++ Assets/Scripts/Enemy/CactusMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class CactusMove : MonoBehaviour
+{
+    private Rigidbody2D _enemyRb;
+    [SerializeField] private float enemyVelocity;
+    
+    void Start()
+    {
+        _enemyRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _enemyRb.velocity = Vector2.left * enemyVelocity;
+    }
+}
