670bd5f3f3b44ce9a43ba2f8974a4c906b34788b
snapp <snapp@snapp.co>
2023-04-17 22:27:02 +0300

    Added spawner, pool, and destroy system. Not working Threads

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading;
+using UnityEngine;
+
+public class EnemySpawner : ObjectPool
+{
+    [SerializeField] private GameObject[] prefabs;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private float timeBetweenSpawnInSeconds;
+    private int _timeBetweenSpawnInMillisecond;
+    private float _timeLeft;
+    
+    private void Start()
+    { 
+        //_timeBetweenSpawnInMillisecond = (int) (_timeBetweemSpawnInSeconds * 1000);
+        Initialize(prefabs);
+       // TimerCallback timerCallback = new TimerCallback(TrySpawnEnemy);
+       //Timer timer = new Timer(timerCallback, 0, 0, _timeBetweenSpawnInMillisecond);
+    }
+
+    private void Update()
+    {
+        _timeLeft += Time.deltaTime;
+        if (_timeLeft >= timeBetweenSpawnInSeconds)
+        {
+            TrySpawnEnemy();
+            _timeLeft = 0;
+        }
+    }
+
+    private void TrySpawnEnemy()
+    {
+        if (TryGetObject(out GameObject enemy))
+        {
+            SetEnemy(enemy);
+        }
+    }
+
+    private void SetEnemy(GameObject enemy)
+    {
+        enemy.SetActive(true);
+        enemy.transform.position = spawnPoint.position;
+    }
+}
---
+++ Assets/Scripts/Enemy/ObjectPool.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField] private GameObject objectContainer;
+    [SerializeField] private int containerCapacity;
+    private readonly System.Random _random = new System.Random();
+    private List<GameObject> pool = new List<GameObject>();
+
+    protected void Initialize(GameObject[] prefab)
+    {
+        for (int i = 0; i < containerCapacity; i++) 
+        { 
+            GameObject spawned = Instantiate(prefab[Random.Range(0, prefab.Length)], objectContainer.transform); 
+            spawned.SetActive(false);
+            pool.Add(spawned);
+        }
+    }
+
+    protected bool TryGetObject(out GameObject result)
+    { 
+     
+        int randomElement = Random.Range(0, containerCapacity);
+    
+        while (!pool[randomElement].activeSelf == false)
+        {   
+          
+            randomElement = Random.Range(0, containerCapacity);
+        }
+       
+        result = pool[randomElement];  //(p => p.activeSelf == false);
+        return result != null;  
+    }
+}
