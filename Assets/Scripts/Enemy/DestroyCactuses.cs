bc388dd7184364aa4ff8dbb84fa0707600122ef6
snapp <snapp@snapp.co>
2023-04-17 19:55:55 +0300

    Added cactuses destroy system

---
+++ Assets/Scripts/Enemy/DestroyCactuses.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DestroyCactuses : MonoBehaviour
+{
+   private void OnTriggerEnter2D(Collider2D colliderToEnter)
+   {
+      if (colliderToEnter.gameObject.TryGetComponent(out CactusMove cactusMove))
+      {
+         colliderToEnter.gameObject.SetActive(false);
+      }
+   }
+}
